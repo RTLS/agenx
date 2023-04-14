adaa6f8537b77708c7da59ca7dcaf3a6d835f5f8
RTLS <carter.e.pedersen@gmail.com>
2023-04-14 08:21:53 -0700

    🎉 Agenx init

---
+++ lib/agenx.ex
@@ -0,0 +1,18 @@
+defmodule Agenx do
+  @moduledoc """
+  Documentation for `Agenx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Agenx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/agenx_test.exs
@@ -0,0 +1,8 @@
+defmodule AgenxTest do
+  use ExUnit.Case
+  doctest Agenx
+
+  test "greets the world" do
+    assert Agenx.hello() == :world
+  end
+end
